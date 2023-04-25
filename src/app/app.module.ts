0a0724895b3a3765b6d5d1e4247c5d8f5da17b15
Амаланий Ион Степанович <iamalaniy@solit-clouds.ru>
2023-04-25 11:25:36 +0300

    feature(mat-toolbar): added MatToolbarModule from angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatToolbarComponent } from './mat-toolbar/mat-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatToolbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mat-toolbar/mat-toolbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-toolbar',
+  templateUrl: './mat-toolbar.component.html',
+  styleUrls: ['./mat-toolbar.component.scss'],
+})
+
+
+export class MatToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
